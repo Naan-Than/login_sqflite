9f78ee4a84e792d3e549050cd92580e49ec68f02
Naan-Than <120182311+Naan-Than@users.noreply.github.com>
2023-03-31 10:12:16 +0530

    name

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 class HomeScreen extends StatefulWidget {
 
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,33 +9,33 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late String name;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-      initDb();
+    initDb();
+
   }
 
   initDb() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.getString("name");
-    prefs.getString("email");
-    prefs.getString("password");
-    prefs.getString("mobile");
-    prefs.getString("address");
-  }
+    setState(() {
+      name = prefs.getString("name")!;
+    });
+     }
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("d"),
+        title: const Text("data"),
         backgroundColor: Colors.purpleAccent,
       ),
       body: Column(
         children: [
-          Text("data"),
+          Text(name),
         ],
       ),
     );
